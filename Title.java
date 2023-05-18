4c58777f540efbaa39e0b5d49856c98b373a4891
MarkovskiMihail <115080568+MarkovskiMihail@users.noreply.github.com>
2023-05-18 17:45:46 +0300

    Add files via upload

---
+++ Title.java
@@ -0,0 +1,33 @@
+
+public class Title {
+	private int id;
+	private String title;
+
+	public Title(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Title [id=" + id + ", title=" + title + "]";
+	}
+
+}
